659d395f6b6d2fcf43e264043a8a3b5f42f4107d
Awtanabe <panser098@gmail.com>
2021-06-12 22:25:12 +0900

    ヘッダー作成

--- organism/Header.js
+++ organism/Header.js
@@ -1,6 +1,35 @@
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="header">
+      <div className="brand">
+        Brand
+      </div>
+      <div className="header-links">
+        <a href="" className="header-link">Home</a>
+        <a href="" className="header-link">Blog</a>
+        <a href="" className="header-link">About us</a>
+      </div>
+      <style jsx>{`
+        .header {
+          display: flex;
+          justify-content: space-between;
+          width: 100%;
+          padding: 20px 20px;
+        }
+
+        .header .brand {
+          font-weight: bold;
+          font-size: 20px;
+        }
+
+        .header-links .header-link{
+          padding: 0 10px;
+          text-decoration: none;
+          color: black;
+        }
+      `}
+      </style>
+    </div>
   )
 }
 
